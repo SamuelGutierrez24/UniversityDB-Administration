0f6f47120c898ec0c338f83fafc76e37d6993f9b
SebasL07 <sebasgnv0207@gmail.com>
2024-05-25 22:54:28 -0500

    doing filter people

--- EventsDB/views/persons.py
+++ EventsDB/views/persons.py
@@ -8,10 +8,25 @@ personasdb = db["Personas"]
 
 
 def listP(request):
-
     personas = personasdb.find()
+
+    if request.method == 'POST':
+        if 'searchBtn' in request.POST:
+            id = request.POST.get('idPerson', None)
+            if id:
+                personas = personas.find({'identificacion': id})
+            else:
+                personas = personas.find()  
+    else:
+        personas = personas.find()
+
     context = {'personas': personas}
-    return render(request,'persons.html', context)
+    return render(request, 'persons.html', context)
+
+
+    
+
+
 
 def add_to_event(request):
     return render(request,'add_to_event.html')
