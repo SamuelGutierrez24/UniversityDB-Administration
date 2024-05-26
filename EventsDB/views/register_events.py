5f7f49e97fe6c24642894d74dd08e50b1e9b87ab
Santiago Espinosa <103274976+Santyet@users.noreply.github.com>
2024-05-25 20:03:15 -0500

    more things on mongo

--- EventsDB/views/register_events.py
+++ EventsDB/views/register_events.py
@@ -6,7 +6,8 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb+srv://project:project@datos.ltwggtv.mongodb.net/?retryWrites=true&w=majority&appName=Datos")
 db = client["Datos"]
-collection = db["Datos"]
+eventos = db["Eventos"]
+lugares = db["Lugares"]
 
 def register1(request):
     if request.method == 'POST':
@@ -14,6 +15,13 @@ def register1(request):
         if form.is_valid():
             fecha = datetime.datetime.strptime(request.POST['fecha'], '%Y-%m-%d').date()
             fecha2 = fecha.isoformat()
+            titulo = request.POST['titulo']
+
+            if eventos.find_one({'titulo': titulo, 'fecha': fecha2}):
+            
+                form.add_error(None, 'El evento con este título y fecha ya existe.')
+                return render(request, 'register_event_pageA.html', {'form': form})
+            
             request.session['r_events_1'] = {
                 'titulo': request.POST['titulo'],
                 'descripcion': request.POST['descripcion'],
@@ -34,12 +42,20 @@ def register2(request):
     if request.method == 'POST':
         form = EventPlaceForm(request.POST)
         if form.is_valid():
+            
+            event_data = request.session.get('r_events_1', {})
+            if eventos.find_one({'titulo': event_data['titulo'], 'fecha': event_data['fecha']}):
+                return render(request, 'menu.html', {'form': form})
+            
             request.session['r_events_2'] = form.cleaned_data
             r_events_1 = request.session.get('r_events_1', {})
             r_events_2 = request.session.get('r_events_2', {})
+            lugar = {**r_events_2}
 
-            combined_data = {**r_events_1, **r_events_2}
-            collection.insert_one(combined_data)
+            lugar_id = lugares.insert_one(lugar).inserted_id
+            evento = {**r_events_1,'lugar': { '_id':lugar_id,**r_events_2}}
+            eventos.insert_one(evento)
+            
             print(request.POST)
             return redirect('menu')
     else:
--- EventsDB/views/register_person.py
+++ EventsDB/views/register_person.py
@@ -1,11 +1,25 @@
 import datetime
 from django.shortcuts import render,redirect
 from EventsDB.forms import *
+from EventsDB.models import Empleado
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb+srv://project:project@datos.ltwggtv.mongodb.net/?retryWrites=true&w=majority&appName=Datos")
+db = client["Datos"]
+personas = db["Personas"]
 
 def register1(request):
     if request.method == 'POST':
         form = UsuarioForm(request.POST)
         if form.is_valid():
+            id = request.POST['identificacion']
+
+            if personas.find_one({'identificacion':id}):
+            
+                form.add_error(None, 'El usuario ya existe.')
+                return render(request, 'register_user.html', {'form': form})
+            
             request.session['r_user_1'] = form.cleaned_data
             print(request.POST)
             return redirect('rUser_2')
@@ -17,10 +31,21 @@ def register2(request):
     if request.method == 'POST':
         form = CityUserForm(request.POST)
         if form.is_valid():
-            r_user_1 = request.session.get('r_user_1', {})
-            r_user_2 = form.cleaned_data
 
+            event_data = request.session.get('r_user_1', {})
+
+            if personas.find_one({'identificacion':event_data['identificacion']}):
+            
+                form.add_error(None, 'El usuario ya existe.')
+                return render(request, 'menu.html', {'form': form})
+            
+            request.session['r_user_2'] = form.cleaned_data
+            r_user_1 = request.session.get('r_user_1', {})
+            r_user_2 = request.session.get('r_user_2', {})
+            combined_data = {**r_user_1, **r_user_2}
+            personas.insert_one(combined_data)
             print(request.POST)
+            return redirect('menu')
     else:
         form = CityUserForm(request.POST)
         return render(request,'register_user2.html',{'form': form})
