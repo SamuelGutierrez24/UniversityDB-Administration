71659487b39fbbd42b518b500c3d55a088860138
Santiago Espinosa <103274976+Santyet@users.noreply.github.com>
2024-05-25 23:36:24 -0500

    buenas noches

--- EventsDB/views/persons.py
+++ EventsDB/views/persons.py
@@ -5,30 +5,20 @@ from pymongo import MongoClient
 client = MongoClient("mongodb+srv://project:project@datos.ltwggtv.mongodb.net/?retryWrites=true&w=majority&appName=Datos")
 db = client["Datos"]
 personasdb = db["Personas"]
+eventosdb = db["Eventos"]
 
 
 def listP(request):
-
     personas = personasdb.find()
     context = {'personas': personas}
-
-    if request.method == 'GET':
-        return render(request,'persons.html', context)
-    else:
-        if 'searchBtn' in request.POST:
-            if 'idPerson' != None:
-                id = request.POST.get('idPerson')
-            
-                personas = personasdb.find({'identificacion':id})
-
-        context = {'personas': personas}
-
-        return render(request,'persons.html', context)
-
-
-    
-
-
+    return render(request,'persons.html', context)
 
 def add_to_event(request):
-    return render(request,'add_to_event.html')
+
+    if request.method == 'POST':
+        id = request.POST["identificacion"]
+        persona = personasdb.find_one({"identificacion": id})
+        
+        eventos = eventosdb.find()
+        context = {'persona': persona, 'eventos': eventos}
+        return render(request,'add_to_event.html', context)
