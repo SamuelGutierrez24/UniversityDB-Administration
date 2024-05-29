ac3f7e5f0a53069c1b617bb1e8d0a7203c13c9f4
SebasL07 <sebasgnv0207@gmail.com>
2024-05-29 18:54:27 -0500

    register of comments done

--- EventsDB/views/comments.py
+++ EventsDB/views/comments.py
@@ -5,12 +5,39 @@ from EventsDB.forms import *
 client = MongoClient("mongodb+srv://project:project@datos.ltwggtv.mongodb.net/?retryWrites=true&w=majority&appName=Datos")
 db = client["Datos"]
 comments_db = db["Comentarios"]
+personas = db["Personas"]
+eventsdb = db["Eventos"]
 
 def filterComments(request):
     forms = CommentsForm(request.POST)
+    comments_info = comments_db.find()
+    context = {'form': forms,
+            'comments': comments_info}
 
     if request.method == 'GET':
-        ...
-    context = {'form': forms}
+        context = {'form': forms,
+                   'comments': comments_info}
+        return render(request,'comments.html',context)
+    else:
+        if forms.is_valid():
+            idPerson = request.POST['idPerson']
+
+            if personas.find({'identificacion':idPerson}) is False:
+                forms.add_error(None, 'El usuario no existe.')
+                return render(request,'comments.html',context)
+            
+            idEvent = request.POST['idEvent']
+
+            if eventsdb.find({'titulo':idEvent}) is False:
+                forms.add_error(None, 'El usuario no existe.')
+                return render(request,'comments.html',context)
+            
+            request.session['r_comment'] = forms.cleaned_data
+
+            comments = request.session.get('r_comment',{})
+
+
+            comments_db.insert_one({**comments})
+            print(request.POST)
+            
     
-    return render(request,'comments.html',context)
